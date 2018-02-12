ee1f8c3fb8e303ae4ea34c05c3f7919d253ae60a
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-02-12 16:35:05 +0200

    Examples

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Test
     {
+        //define a delegate		
+        public delegate int CalcHandler(int x, int y);
+
+        //test using a delegate
         static void Main(string[] args)
         {
+            MyMath math = new MyMath();
+            //create a new instance of the delegate class
+            CalcHandler sumHandler = new CalcHandler(math.Sum);
+            //invoke the delegate
+            int result = sumHandler(4, 2);
+            Console.WriteLine("Result is: " + result);
+            //change the method and invoke again
+            sumHandler = new CalcHandler(math.FakeSum);
+            Console.WriteLine("Result is now: " + sumHandler(4, 2));
         }
+    }
+
+    public class MyMath
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int FakeSum(int x, int y)
+        {
+            return int.Parse(x.ToString() + y.ToString());
+        }
+
     }
 }
---
+++ Esim2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esim2
+{
+    class DelegaattiDemo
+    {
+        delegate void FormatoiLuku(double luku);
+
+        public static void Main(string[] args)
+        {
+            //kysytään käyttäjältä jokin desimaaliluku
+            Console.WriteLine("Anna desimaaliluku");
+            string syote = Console.ReadLine();
+            double aluku;
+            if (double.TryParse(syote, out aluku))
+            {
+                //kiinnitetään useita metodeja delegaatille
+                FormatoiLuku formatoija = FormatoiValuutaksi;
+                formatoija += FormatoiKolmelleDesimaalille;
+                //kutsutaan kiinnitetyt delegaatit
+                formatoija(aluku);
+            }
+
+            List<string> nimet = new List<string> { "Arska", "Kalle", "Cecilia", "Jack" };
+            string foo = nimet.FirstOrDefault(x => x.StartsWith("Ja"));
+            Console.WriteLine(foo); //this prints Jack
+        }
+
+        //metodit
+        static void FormatoiValuutaksi(double luku)
+        {
+            Console.WriteLine("Annettu luku valuuttana {0:C}", luku);
+        }
+        static void FormatoiKolmelleDesimaalille(double luku)
+        {
+            Console.WriteLine("Annettu luku kolmen desimaalin tarkkuudella {0:.###}", luku);
+        }
+
+    }
+}
